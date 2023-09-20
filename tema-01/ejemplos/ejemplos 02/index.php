a6f05549381905f4acb5c63c83c0d57b63a762d4
Julian Garcia Velazquez <jgarvelju076@g.educaand.es>
2023-09-20 14:49:00 +0200

    first commit

---
+++ tema-01/ejemplos/ejemplos 02/index.php
@@ -0,0 +1,22 @@
+<?php
+    /*
+
+    Ejemplo 2 - PHP
+    1º Commit github
+
+    */
+
+    echo "<h1>";
+    echo "<hr>";
+    echo "<center>";
+    echo "Hola Juan Carlos";
+    echo "<BR>";
+    echo "Soy Julian";
+    echo "<BR>";
+    echo "Este es mi prmier commit em dsew";
+    echo '</center>';
+    echo '<hr>';
+    echo '</h1>';
+    echo 'DWES - Curso 23/24';
+
+?>
