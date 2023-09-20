ae5c4b77ab1d80140028a2603ae5f1036d720fa2
Julian Garcia Velazquez <jgarvelju076@g.educaand.es>
2023-09-20 14:44:45 +0200

    first commit

---
+++ tema-01/ejemplos/ejemplos 01/index.php
@@ -0,0 +1,20 @@
+<?php
+    /*
+
+    Ejemplo 1 - PHP
+    Hola Mundo
+
+    */
+
+    echo "<h1>";
+    echo "<hr>";
+    echo "<center>";
+    echo "Hola Mundo PHP";
+    echo "<BR>";
+    echo "Soy Julian";
+    echo '</center>';
+    echo '<hr>';
+    echo '</h1>';
+    echo 'DWES - Curso 23/24';
+
+?>
