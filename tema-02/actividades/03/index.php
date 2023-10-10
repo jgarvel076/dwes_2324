c42618d0569965b9fbdc5eb85fd1fe13bb05d7a7
Julian Garcia Velazquez <jgarvelju076@g.educaand.es>
2023-10-10 09:13:32 +0200

    tema 2 finalizado

--- tema-02/actividades/03/index.php
+++ tema-02/actividades/03/index.php
@@ -1,5 +1,5 @@
 
-<?php
+<?php 
 // Ejercicio 1: Conversiones de datos en expresiones
 
 // Multiplica valor entero con una cadena que contiene un número inicial
@@ -8,30 +8,36 @@ $cadenaNumero = "98";
 $resultado = $valorEntero * $cadenaNumero;
 var_dump(gettype($resultado));
 
+?>
+<?php   
 // Sumar valor entero con cadena con número inicial
 $valorEntero = 8;
 $cadenaNumero = "15";
 $resultado = $valorEntero + $cadenaNumero;
 var_dump(gettype($resultado));
-
+?>
+<?php
 // Sumar valor entero con valor float
 $valorEntero = 10;
 $valorFloat = 3.5;
 $resultado = $valorEntero + $valorFloat;
 var_dump(gettype($resultado));
-
+?>
+<?php
 // Concatenar valor entero con cadena
 $valorEntero = 15;
 $cadena = "Hola ";
 $resultado = $cadena . $valorEntero;
 var_dump(gettype($resultado));
-
+?>
+<?php
 // Sumar valor entero con valor booleano
 $valorEntero = 7;
 $valorBooleano = true;
 $resultado = $valorEntero + $valorBooleano;
 var_dump(gettype($resultado));
-
+?>
+<?php
 // Ejercicio 2: is_null()
 
 $valor1 = null;
@@ -42,12 +48,13 @@ $valor5 = 0;
 $valor6 = array();
 
 var_dump(is_null($valor1) );
-var_dump(is_null($valor2) ?);
+var_dump(is_null($valor2) );
 var_dump(is_null($valor3));
 var_dump(is_null($valor4));
 var_dump(is_null($valor5));
 var_dump(is_null($valor6));
-
+?>
+<?php
 // Ejercicio 3: isset()
 
 $valor1 = null;
@@ -63,7 +70,8 @@ var_dump(isset($valor3));
 var_dump(isset($valor4));
 var_dump(isset($valor5));
 var_dump(isset($valor6));
-
+?>
+<?php
 // Ejercicio 4: empty()
 
 $valor1 = null;
@@ -79,5 +87,5 @@ var_dump(empty($valor3));
 var_dump(empty($valor4));
 var_dump(empty($valor5));
 var_dump(empty($valor6));
-?>
 
+?>
