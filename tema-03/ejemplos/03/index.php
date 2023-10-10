715581e54dcbf431d93ab9de37ce3757a74f922c
Julian Garcia Velazquez <jgarvelju076@g.educaand.es>
2023-10-10 10:15:15 +0200

    tema 3

---
+++ tema-03/ejemplos/03/index.php
@@ -0,0 +1,17 @@
+<?php
+
+$a = 1;
+
+if ($a < 5){
+    echo "insuficiente";
+}elseif ($a < 6){
+    echo "suficiente";
+}elseif ($a < 7){
+    echo "bien";
+}elseif ($a < 9){
+    echo "notable";
+}elseif ($a >9){
+    echo "sobresaliente";
+}
+
+?>
