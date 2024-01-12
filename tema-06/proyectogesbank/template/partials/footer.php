9d1e2003e4afe257f1ee0d241b27aa3dff9c94d1
jgarvel076 <jgarvel076@g.educaand.es>
2024-01-12 13:26:49 +0100

    fin proyecto

--- tema-06/proyectogesbank/template/partials/footer.php
+++ tema-06/proyectogesbank/template/partials/footer.php
@@ -5,6 +5,6 @@
 <footer class="footer mt-auto py-3 fixed-bottom bg-light">
   <div class="container">
     <span class="text-muted">© 2024 
-        Jorge Coronil Villalba - DWES - 2º DAW - Curso 23/24</span>
+        Julian Garcia Velazquez - DWES - 2º DAW - Curso 23/24</span>
   </div>
 </footer>
