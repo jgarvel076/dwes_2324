f63200bad1ef89d9356b8a4291bab406b0c29991
Julian Garcia Velazquez <jgarvelju076@g.educaand.es>
2024-01-15 14:39:52 +0100

    7.2

--- tema-07/7.2 - Validación Formularios/template/partials/footer.php
+++ tema-07/7.2 - Validación Formularios/template/partials/footer.php
@@ -5,7 +5,7 @@
 <footer class="footer mt-auto py-3 fixed-bottom bg-light">
   <div class="container">
     <span class="text-muted">© 2024 
-        Pablo Mateos Palas - DWES - 2º DAW - Curso 23/24
+        Julian Garcia Velazquez - DWES - 2º DAW - Curso 23/24
     </span>
   </div>
 </footer>
