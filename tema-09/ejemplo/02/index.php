ca14004760ed5d65cc9a67b4960b542f194de1be
Julian Garcia Velazquez <jgarvelju076@g.educaand.es>
2024-02-06 09:56:38 +0100

    cosas

---
+++ tema-09/ejemplo/02/index.php
@@ -0,0 +1,7 @@
+<?php 
+    require ('fpdf/fpdf.php');
+    $pdf=new FPDF ();
+    $pdf->AddPage();
+    $pdf->SetFont('Arial','B',16);
+    $pdf->Cell(40,10, incov('UTF-8', 'ISO-8859-1','¡Mi primera página pdf con FPDF!'));
+    $pdf->Output();
