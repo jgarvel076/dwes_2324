3270a100b4174c67610f14eab57a6fa70c0ed904
Julian Garcia Velazquez <jgarvelju076@g.educaand.es>
2024-02-12 14:26:31 +0100

    proyecto 8.2

---
+++ tema-08/proyecto/8.2/config/config.php
@@ -0,0 +1,15 @@
+<?php
+# Configuración básica aplicación MVC
+
+# Ruta absoluta
+define('URL', 'http://localhost/DWES/tema-08/proyecto/8.2/');
+
+# Constante de la Base de Datos
+define('HOST', 'localhost');
+define('DB', 'gesbank');
+define('USER', 'root');
+define('PASSWORD', '');
+define('CHARSET', 'utf8mb4');
+
+
+?>
---
+++ tema-10/ejemplos/02/index.php
@@ -0,0 +1,19 @@
+<?php
+
+//Cabecera
+    $header = "Mime-Version: 1.0"."\n";
+    $header .= "Content-Type: text/html; charset-iso-8859-1"."\n";
+    $header .= "From: Julian Garcia Velazquez <jgarvel076@g.educaand.es>\n";
+    $header .= "Cc: jgarvel076@g.educaand.es\n";
+    $header .= "Cco: jgarvel076@g.educaand.es\n";
+    $header .= "X-Mailer: PHP/" . phpversion();
+
+//Parametros
+    $destino = "jgarvel076@g.educaand.es";
+    $asunto = "Prueba de envío de correo electrónico con PHP";
+    $mensaje = "<h2>Este es un mensaje HTML</h2>";
+
+//Enviar el correo
+    if (mail($destino, $asunto, $mensaje, $header)) {
+        echo "Mensaje enviado correstamente";
+    }
