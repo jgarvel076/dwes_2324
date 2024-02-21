cd80c005308807f43615d756e3364900b1edca65
jgarvel076 <jgarvel076@g.educaand.es>
2024-02-21 21:21:35 +0100

    proyectos

---
+++ tema-09/proyecto/proyectopdf/config/config.php
@@ -0,0 +1,15 @@
+<?php
+# Configuración básica aplicación MVC
+
+# Ruta absoluta
+define('URL', 'http://localhost/DWES/dwes_2324/tema-09/proyecto/proyectopdf/');
+
+# Constante de la Base de Datos
+define('HOST', 'localhost');
+define('DB', 'gesbank');
+define('USER', 'root');
+define('PASSWORD', '');
+define('CHARSET', 'utf8mb4');
+
+
+?>
---
+++ tema-09/proyecto/proyectopdf/models/perfilModel.php
@@ -0,0 +1,162 @@
+<?php 
+    class PerfilModel extends Model {
+
+    public function getUserId($id) {
+        try {
+
+            $sql = "SELECT * FROM gesbank.users WHERE id= :id LIMIT 1";
+            $conexion = $this->db->connect();
+            $result = $conexion->prepare($sql);
+            $result->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE,'User');
+            $result->bindParam(":id", $id, PDO::PARAM_INT);
+            $result->execute();
+            
+            return $result->fetch();
+
+        }  catch (PDOException $e) {
+            
+            include_once('template/partials/errorDB.php');
+            exit();
+
+        }
+
+    }
+
+
+    public function updatePass (User $user) {
+        try {
+             
+            $password_encriptado = password_hash($user->password, CRYPT_BLOWFISH);
+            $update = "
+                        UPDATE gesbank.users SET
+                            password = :password
+                        WHERE id = :id      
+                        ";
+
+            $conexion = $this->db->connect();
+            $result = $conexion->prepare($update);
+
+            $result->bindParam(':password', $password_encriptado , PDO::PARAM_STR, 50);
+            $result->bindParam(':id', $user->id, PDO::PARAM_INT) ;
+
+            $result->execute();
+
+        }  catch (PDOException $e) {
+            
+            include_once('template/partials/errorDB.php');
+            exit();
+
+        }
+    }
+
+    public function validateName($name) {
+
+        try {
+            $sql = "
+                    SELECT * FROM gesbank.users
+                    WHERE name = :name
+            ";
+
+            # Conectamos con la base de datos
+            $conexion = $this->db->connect();
+    
+            # Ejecutamos mediante prepare la consulta SQL
+            $result= $conexion->prepare($sql);
+            $result->bindParam(':name', $name, PDO::PARAM_STR);
+            $result -> execute();
+
+           if ($result->rowCount() == 0) 
+                    return TRUE;
+            return FALSE;
+
+        } catch(PDOException $e) {
+            include_once('template/partials/errorDB.php');
+            exit();
+        }
+        
+    
+    }
+
+
+   public function validateEmail($email) {
+
+    try {
+        $sql = "
+                SELECT * FROM gesbank.users
+                WHERE email = :email
+        ";
+
+        # Conectamos con la base de datos
+        $conexion = $this->db->connect();
+
+        # Ejecutamos mediante prepare la consulta SQL
+        $result= $conexion->prepare($sql);
+        $result->bindParam(':email', $email, PDO::PARAM_STR);
+        $result -> execute();
+
+        if ($result->rowCount() == 0) 
+            return TRUE;
+        return FALSE;
+
+    } catch(PDOException $e) {
+        include_once('template/partials/errorDB.php');
+        exit();
+    }
+    
+    }
+
+    public function update (User $user) {
+        try {
+                     
+            $update = "
+                        UPDATE gesbank.users SET
+                            name = :name,
+                            email = :email
+                        WHERE id = :id
+                        LIMIT 1      
+                        ";
+
+            $conexion = $this->db->connect();
+            $result = $conexion->prepare($update);
+
+            $result->bindParam(':name', $user->name, PDO::PARAM_STR, 50);
+            $result->bindParam(':email', $user->email , PDO::PARAM_STR, 50);
+            $result->bindParam(':id', $user->id, PDO::PARAM_INT) ;
+
+            $result->execute();
+
+        }  catch (PDOException $e) {
+            
+            include_once('template/partials/errorDB.php');
+            exit();
+
+        }
+    }
+
+
+    public function delete($id) {
+
+        try {
+                $delete = "
+                    DELETE FROM gesbank.users 
+                    WHERE id = :id      
+                ";
+
+                $conexion = $this->db->connect();
+                $result = $conexion->prepare($delete);
+
+                $result->bindParam(':id', $id, PDO::PARAM_INT) ;
+
+                $result->execute();
+
+        }  catch (PDOException $e) {
+
+            include_once('template/partials/errorDB.php');
+            exit();
+
+        }
+
+    }
+
+}
+?>
