752b38d4a43142cc90e540db9ecda7f8ac7ab905
jgarvel076 <jgarvel076@g.educaand.es>
2024-03-07 20:23:52 +0100

    proyecto 10

---
+++ tema-10/proyecto/proyectomail/models/clientesModel.php
@@ -0,0 +1,349 @@
+<?php
+
+
+class clientesModel extends Model
+{
+
+    # Método get
+    public function get()
+    {
+        try {
+            $sql = "
+
+            SELECT 
+                id,
+                concat_ws(', ', apellidos, nombre) cliente,
+                telefono,
+                ciudad,
+                dni,
+                email
+            FROM 
+                clientes
+            ORDER BY id;
+
+            ";
+
+            $conexion = $this->db->connect();
+            $pdoSt = $conexion->prepare($sql);
+            $pdoSt->setFetchMode(PDO::FETCH_OBJ);
+            $pdoSt->execute();
+            return $pdoSt;
+
+        } catch (PDOException $e) {
+            require_once("template/partials/errorDB.php");
+            exit();
+        }
+    }
+
+    # Método create
+    public function create(Cliente $cliente)
+    {
+        try {
+            $sql = " INSERT INTO 
+                        clientes 
+                        (
+                            nombre, 
+                            apellidos, 
+                            email, 
+                            telefono, 
+                            ciudad, 
+                            dni
+                        ) 
+                        VALUES 
+                        ( 
+                            :nombre,
+                            :apellidos,
+                            :email,
+                            :telefono,
+                            :ciudad,
+                            :dni
+                        )";
+
+            $conexion = $this->db->connect();
+            $pdoSt = $conexion->prepare($sql);
+
+            //Vinculamos los parámetros
+            $pdoSt->bindParam(":nombre", $cliente->nombre, PDO::PARAM_STR, 30);
+            $pdoSt->bindParam(":apellidos", $cliente->apellidos, PDO::PARAM_STR, 50);
+            $pdoSt->bindParam(":email", $cliente->email, PDO::PARAM_STR, 50);
+            $pdoSt->bindParam(":telefono", $cliente->telefono, PDO::PARAM_STR, 9);
+            $pdoSt->bindParam(":ciudad", $cliente->ciudad, PDO::PARAM_STR, 30);
+            $pdoSt->bindParam(":dni", $cliente->dni, PDO::PARAM_STR, 9);
+
+            // ejecuto
+            $pdoSt->execute();
+
+        } catch (PDOException $e) {
+            require_once("template/partials/errorDB.php");
+            exit();
+        }
+    }
+
+    # Método delete
+    public function delete($id)
+    {
+        try {
+
+            $sql = " 
+                   DELETE FROM clientes WHERE id = :id;
+                   ";
+
+            $conexion = $this->db->connect();
+            $pdoSt = $conexion->prepare($sql);
+            $pdoSt->bindParam(":id", $id, PDO::PARAM_INT);
+            $pdoSt->execute();
+            return $pdoSt;
+
+        } catch (PDOException $error) {
+            require_once("template/partials/errorDB.php");
+            exit();
+        }
+    }
+
+    # Método getCliente
+    public function getCliente($id)
+    {
+        try {
+            $sql = " 
+                    SELECT     
+                        id,
+                        apellidos,
+                        nombre,
+                        telefono,
+                        ciudad,
+                        dni,
+                        email
+                    FROM  
+                        clientes  
+                    WHERE
+                        id = :id";
+
+            $conexion = $this->db->connect();
+            $pdoSt = $conexion->prepare($sql);
+            $pdoSt->bindParam(":id", $id, PDO::PARAM_INT);
+            $pdoSt->setFetchMode(PDO::FETCH_OBJ);
+            $pdoSt->execute();
+            return $pdoSt->fetch();
+
+        } catch (PDOException $e) {
+            require_once("template/partials/errorDB.php");
+            exit();
+        }
+    }
+
+    # Método update
+    public function update(Cliente $cliente, $id)
+    {
+        try {
+            $sql = " 
+                    UPDATE clientes
+                    SET
+                        apellidos=:apellidos,
+                        nombre=:nombre,
+                        telefono=:telefono,
+                        ciudad=:ciudad,
+                        dni=:dni,
+                        email=:email,
+                        update_at = now()
+                    WHERE
+                        id=:id
+                    LIMIT 1";
+
+            $conexion = $this->db->connect();
+            $pdoSt = $conexion->prepare($sql);
+            //Vinculamos los parámetros
+            $pdoSt->bindParam(":nombre", $cliente->nombre, PDO::PARAM_STR, 30);
+            $pdoSt->bindParam(":apellidos", $cliente->apellidos, PDO::PARAM_STR, 50);
+            $pdoSt->bindParam(":email", $cliente->email, PDO::PARAM_STR, 50);
+            $pdoSt->bindParam(":telefono", $cliente->telefono, PDO::PARAM_STR, 9);
+            $pdoSt->bindParam(":ciudad", $cliente->ciudad, PDO::PARAM_STR, 30);
+            $pdoSt->bindParam(":dni", $cliente->dni, PDO::PARAM_STR, 9);
+            $pdoSt->bindParam(":id", $id, PDO::PARAM_INT);
+
+            $pdoSt->execute();
+
+        } catch (PDOException $error) {
+            require_once("template/partials/errorDB.php");
+            exit();
+        }
+    }
+
+
+
+    # Método update
+    public function order(int $criterio)
+    {
+        try {
+            $sql = "
+                    SELECT 
+                        id,
+                        concat_ws(', ', apellidos, nombre) cliente,
+                        telefono,
+                        ciudad,
+                        dni,
+                        email
+                    FROM 
+                        clientes 
+                    ORDER BY
+                        :criterio";
+
+            $conexion = $this->db->connect();
+            $pdoSt = $conexion->prepare($sql);
+            $pdoSt->bindParam(":criterio", $criterio, PDO::PARAM_INT);
+            $pdoSt->setFetchMode(PDO::FETCH_OBJ);
+
+            $pdoSt->execute();
+
+            return $pdoSt;
+        } catch (PDOException $e) {
+            require_once("template/partials/errorDB.php");
+            exit();
+        }
+    }
+
+    # Método filter
+    public function filter($expresion)
+    {
+        try {
+  
+            $sql = "
+                    SELECT 
+                        id,
+                        concat_ws(', ', apellidos, nombre) cliente,
+                        telefono,
+                        ciudad,
+                        dni,
+                        email
+                    FROM 
+                        clientes 
+                    WHERE 
+                        concat_ws(  
+                                    ' ',
+                                    id,
+                                    apellidos,
+                                    nombre,
+                                    telefono,
+                                    ciudad,
+                                    dni,
+                                    email
+                                )
+                        LIKE 
+                                :expresion
+                    
+                    ORDER BY id ASC";
+
+            $conexion = $this->db->connect();
+            $pdoSt = $conexion->prepare($sql);
+
+            # enlazamos parámetros con variable
+            $expresion = "%" . $expresion . "%";
+            $pdoSt->bindValue(':expresion', $expresion, PDO::PARAM_STR);
+
+            $pdoSt->setFetchMode(PDO::FETCH_OBJ);
+            $pdoSt->execute();
+            return $pdoSt;
+
+        } catch (PDOException $e) {
+            require_once("template/partials/errorDB.php");
+            exit();
+        }
+    }
+
+    public function validateUniqueDni($dni){
+        try {
+            // Creamos la sentencia
+            $sql = "SELECT * FROM gesbank.clientes WHERE dni = :dni";
+
+            // Nos conectamos a la base de datos
+            $conexion = $this->db->connect();
+
+            // Preparamos la consulta
+            $pdostmt = $conexion->prepare($sql);
+
+            // Vinculamos la variable
+            $pdostmt->bindParam(':dni', $dni, PDO::PARAM_STR);
+
+            // Ejecutamos la sentencia
+            $pdostmt->execute();
+
+            if($pdostmt->rowCount() != 0){
+                return false;
+            }
+            return true;
+        } catch (PDOException $e) {
+            require_once("template/partials/errorDB.php");
+            exit();
+        }
+    }
+
+    public function validateUniqueEmail($email){
+        try {
+            // Creamos la sentencia
+            $sql = "SELECT * FROM gesbank.clientes WHERE email = :email";
+
+            // Nos conectamos a la base de datos
+            $conexion = $this->db->connect();
+
+            // Preparamos la consulta
+            $pdostmt = $conexion->prepare($sql);
+
+            // Vinculamos la variable
+            $pdostmt->bindParam(':email', $email, PDO::PARAM_STR);
+
+            // Ejecutamos la sentencia
+            $pdostmt->execute();
+
+            if($pdostmt->rowCount() != 0){
+                return false;
+            }
+            return true;
+        } catch (PDOException $e) {
+            require_once("template/partials/errorDB.php");
+            exit();
+        }
+    }
+
+    function getCSV()
+    {
+
+        try {
+
+            # comando sql
+            $sql = "SELECT 
+                        clientes.id,
+                        clientes.apellidos,
+                        clientes.nombre,
+                        clientes.email,
+                        clientes.telefono,
+                        clientes.ciudad,
+                        clientes.dni
+                    FROM
+                        clientes
+                    ORDER BY 
+                        id";
+
+            # conectamos con la base de datos
+
+            // $this->db es un objeto de la clase database
+            // ejecuto el método connect de esa clase
+
+            $conexion = $this->db->connect();
+
+            # ejecutamos mediante prepare
+            $pdost = $conexion->prepare($sql);
+
+            # establecemos  tipo fetch
+            $pdost->setFetchMode(PDO::FETCH_OBJ);
+
+            #  ejecutamos 
+            $pdost->execute();
+
+            # devuelvo objeto pdostatement
+            return $pdost;
+        } catch (PDOException $e) {
+
+            include_once('template/partials/errorDB.php');
+            exit();
+        }
+    }
+}
