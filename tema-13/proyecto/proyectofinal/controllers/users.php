36672ecf6a697a37222bf0777f419fddefa651db
jgarvel076 <jgarvel076@g.educaand.es>
2024-03-10 23:56:53 +0100

    users

---
+++ tema-13/proyecto/proyectofinal/controllers/users.php
@@ -0,0 +1,520 @@
+<?php
+require_once 'class/class.user.php';
+
+
+class Users extends Controller
+{
+
+    function __construct()
+    {
+
+        parent::__construct();
+
+
+    }
+
+    function render($param = [])
+    {
+
+        # Inicio o continúo la sesión
+        session_start();
+
+        //Comprobar si el usuario está identificado
+        if (!isset($_SESSION['id'])) {
+            $_SESSION['mensaje'] = "Usuario No Autentificado";
+
+            header("location:" . URL . "login");
+        } else if ((!in_array($_SESSION['id_rol'], $GLOBALS['user']['main']))) {
+            $_SESSION['mensaje'] = "Operación sin privilegios";
+            header('location:' . URL . 'users');
+        } else {
+
+            # Comprobar si existe el mensaje
+            if (isset($_SESSION['mensaje'])) {
+                $this->view->mensaje = $_SESSION['mensaje'];
+                unset($_SESSION['mensaje']);
+            }
+            $this->view->model = $this->model;
+            # Creo la propiedad title de la vista
+            $this->view->title = "Tabla Usuarios";
+
+            # Creo la propiedad users dentro de la vista
+            # Del modelo asignado al controlador ejecuto el método get();
+            $this->view->users = $this->model->get();
+            $this->view->render("users/main/index");
+        }
+    }
+
+    function new()
+    {
+        # Iniciar o continuar sesión
+        session_start();
+
+        //Comprobar si el usuario está identificado
+        if (!isset($_SESSION['id'])) {
+            $_SESSION['mensaje'] = "Usuario no autentificado";
+
+            header("location:" . URL . "login");
+        } else if ((!in_array($_SESSION['id_rol'], $GLOBALS['user']['new']))) {
+            $_SESSION['mensaje'] = "Operación sin privilegios";
+            header('location:' . URL . 'users');
+        }
+
+        // Obtener los roles
+        $roles = $this->model->getRoles();
+
+        // Pasar los roles a la vista
+        $this->view->roles = $roles;
+
+        # Comprobar si vuelvo de un registro no validado
+        if (isset($_SESSION['error'])) {
+            # Mensaje de error
+            $this->view->error = $_SESSION['error'];
+
+            # Autorrellenar el formulario con los detalles del usuario
+            $this->view->user = unserialize($_SESSION['user']);
+
+            # Recupero array de errores específicos
+            $this->view->errores = $_SESSION['errores'];
+
+            unset($_SESSION['error']);
+            unset($_SESSION['errores']);
+            unset($_SESSION['user']);
+        }
+
+        # etiqueta title de la vista
+        $this->view->title = "Añadir - Gestión Usuarios";
+
+        # cargo la vista con el formulario nuevo user
+        $this->view->render('users/new/index');
+    }
+
+
+    public function validate()
+    {
+
+        # Iniciamos o continuamos con la sesión
+        session_start();
+
+        # Saneamos el formulario
+        $name = filter_var($_POST['name'], FILTER_SANITIZE_SPECIAL_CHARS);
+        $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+        $password = filter_var($_POST['password'], FILTER_SANITIZE_SPECIAL_CHARS);
+        $password_confirm = filter_var($_POST['password_confirm'], FILTER_SANITIZE_SPECIAL_CHARS);
+        $rol = filter_var($_POST['rol'], FILTER_SANITIZE_SPECIAL_CHARS);
+
+        # Validaciones
+
+        $errores = array();
+
+        # Validar name
+        if (empty($name)) {
+            $errores['name'] = "Campo Obligatorio";
+        } else if (!$this->model->validateName($name)) {
+            $errores['name'] = "Nombre de usuario no válido";
+        }
+
+        # Validar Email
+        if (empty($email)) {
+            $errores['email'] = "Campo Obligatorio.";
+        } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $errores['email'] = "Email: Email no válido";
+        } elseif (!$this->model->validateEmailUnique($email)) {
+            $errores['email'] = "Email existente, ya está registrado";
+        }
+
+        # Validar password
+        if (empty($password)) {
+            $errores['password'] = "Debe introducir una contraseña.";
+        } else if (strcmp($password, $password_confirm) !== 0) {
+            $errores['password'] = "Password no coincidentes";
+        } else if (!$this->model->validatePass($password)) {
+            $errores['password'] = "Password: No permitido";
+        }
+
+        # Validar rol
+        if (empty($rol)) {
+            $errores['rol'] = "Debe seleccionar un rol.";
+        }
+
+        if (!empty($errores)) {
+
+            $_SESSION['errores'] = $errores;
+            $_SESSION['name'] = $name;
+            $_SESSION['email'] = $email;
+            $_SESSION['password'] = $password;
+            $_SESSION['error'] = "Fallo en la validación del formulario";
+
+            header("location:" . URL . "users/new");
+
+        } else {
+
+            # Añade nuevo usuario
+            $this->model->crear($name, $email, $password, $rol);
+
+            $_SESSION['mensaje'] = "Usuario registrado correctamente";
+
+            header("location:" . URL . "users");
+            exit();
+        }
+
+    }
+
+
+    function create($param = [])
+    {
+        // Iniciar sesión
+        session_start();
+
+        if (!isset($_SESSION['id'])) {
+            $_SESSION['mensaje'] = "Usuario no autentificado";
+
+            header("location:" . URL . "login");
+        } else if ((!in_array($_SESSION['id_rol'], $GLOBALS['user']['new']))) {
+            $_SESSION['mensaje'] = "Operación sin privilegios";
+            header('location:' . URL . 'users');
+        }
+
+        // 1. Seguridad. Saneamos los datos del formulario
+        $nombre = filter_var($_POST['nombre'] ??= '', FILTER_SANITIZE_SPECIAL_CHARS);
+        $email = filter_var($_POST['email'] ??= '', FILTER_SANITIZE_EMAIL);
+        $password = filter_var($_POST['password'] ??= '', FILTER_SANITIZE_SPECIAL_CHARS);
+        $rol = filter_var($_POST['rol'] ??= '', FILTER_SANITIZE_NUMBER_INT);
+
+        # Cargamos los datos del formulario
+        $user = new classUser(
+            null,
+            $nombre,
+            $email,
+            $password,
+            $rol
+        );
+
+        // Validación
+        $errores = [];
+
+        // Nombre
+        if (empty($nombre)) {
+            $errores['nombre'] = 'Nombre del usuario obligatorio';
+        } elseif (strlen($nombre) > 20) {
+            $errores['nombre'] = 'El nombre no puede tener más de 20 caracteres';
+        }
+
+        // Email
+        if (empty($email)) {
+            $errores['email'] = 'Email obligatorio';
+        } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $errores['email'] = 'Email inválido';
+        } elseif ($this->model->existeEmail($email, $user->id)) {
+            $errores['email'] = 'Este email ya está registrado';
+        }
+
+        // Password
+        if (empty($password)) {
+            $errores['password'] = 'Contraseña obligatoria';
+        } elseif (!filter_var($password, FILTER_SANITIZE_SPECIAL_CHARS)) {
+            $errores['password'] = 'Contraseña inválida';
+        }
+
+        // Rol
+        if (empty($rol)) {
+            $errores['rol'] = 'Rol obligatorio';
+        }
+
+        // Comprobar validación
+        if (!empty($errores)) {
+            // Errores de validación
+            // Transforma el objeto user en un string
+            $_SESSION['user'] = serialize($user);
+            $_SESSION['error'] = 'Formulario no validado';
+            $_SESSION['errores'] = $errores;
+
+            // Redireccionamos a new
+            header('Location:' . URL . 'users/new');
+            exit();
+        } else {
+
+            // Añadir registro a la tabla
+            $this->model->crear($user);
+
+            // Añadimos a la variable de sesión un mensaje
+            $_SESSION['mensaje'] = 'Usuario creado correctamente';
+
+            // Redirigimos al main de users
+            header('location:' . URL . 'users');
+        }
+    }
+
+
+    function edit($param = [])
+    {
+        session_start();
+
+        if (!isset($_SESSION['id'])) {
+            $_SESSION['mensaje'] = "Usuario no autentificado";
+
+            header("location:" . URL . "login");
+        } else if ((!in_array($_SESSION['id_rol'], $GLOBALS['user']['edit']))) {
+            $_SESSION['mensaje'] = "Operación sin privilegios";
+            header('location:' . URL . 'users');
+        }
+
+        $id = $param[0];
+
+        # asigno id a una propiedad de la vista
+        $this->view->id = $id;
+
+        # title
+        $this->view->title = "Edit - Panel de control users";
+
+        # obtener objeto de la clase user
+        $this->view->user = $this->model->read($id);
+
+        // Obtener los roles
+        $this->view->roles = $this->model->getRoles();
+
+        // Obtener el rol actual del usuario
+        $userRol = $this->model->userRol($id);
+        $this->view->rolActual = $userRol->id;
+
+        //Comprobar si el formulario viene de una validación
+        //Comprobar si el formulario viene de una validación
+        if (isset($_SESSION['error'])) {
+            # Mensaje de error
+            $this->view->error = $_SESSION['error'];
+
+            # Recupero array de errores específicos
+            $this->view->errores = $_SESSION['errores'];
+
+            // Deserializar el objeto classUser solo si no hay errores
+            if (!isset($this->view->errores)) {
+                # Autorrellenar el formulario con los detalles del user
+                $this->view->user = unserialize($_SESSION['user']);
+            }
+
+            unset($_SESSION['error']);
+            unset($_SESSION['errores']);
+            unset($_SESSION['user']);
+        }
+
+
+        # cargo la vista
+        $this->view->render('users/edit/index');
+
+    }
+
+
+    function update($param = [])
+    {
+
+        session_start();
+
+        if (!isset($_SESSION['id'])) {
+            $_SESSION['mensaje'] = "Usuario no autentificado";
+
+            header("location:" . URL . "login");
+        } else if ((!in_array($_SESSION['id_rol'], $GLOBALS['user']['edit']))) {
+            $_SESSION['mensaje'] = "Operación sin privilegios";
+            header('location:' . URL . 'users');
+        }
+
+        # Cargo id del user
+        $id = $param[0];
+
+        $userOG = $this->model->getUser($id);
+
+        // 1. Seguridad. Saneamos los datos del formulario
+        $nombre = filter_var($_POST['name'] ?? '', FILTER_SANITIZE_SPECIAL_CHARS);
+        $email = filter_var($_POST['email'] ?? '', FILTER_SANITIZE_EMAIL);
+        $password = filter_Var($_POST['password'] ?? '', FILTER_SANITIZE_SPECIAL_CHARS);
+        $password_confirm = filter_Var($_POST['password_confirm'] ?? '', FILTER_SANITIZE_SPECIAL_CHARS);
+        $rol = filter_var($_POST['rol'] ?? '', FILTER_SANITIZE_NUMBER_INT);
+
+        // Validación
+        $errores = [];
+
+        // Nombre
+        if (empty($nombre)) {
+            $errores['nombre'] = 'Nombre del usuario obligatorio';
+        } elseif (strlen($nombre) > 20) {
+            $errores['nombre'] = 'El nombre no puede tener más de 20 caracteres';
+        }
+
+        // Email
+        if (empty($email)) {
+            $errores['email'] = 'Email obligatorio';
+        } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $errores['email'] = 'Email inválido';
+        } elseif ($this->model->validateEmailUnique($email, $id)) {
+            $errores['email'] = 'Este email ya está registrado';
+        }
+
+        // Password
+        if (!empty($password) || !empty($password_confirm)) {
+            if (empty($password)) {
+                $errores['password'] = 'Contraseña obligatoria';
+            } elseif ($password !== $password_confirm) {
+                $errores['password_confirm'] = 'Las contraseñas no coinciden';
+            }
+        }
+
+        // Rol
+        if (empty($rol)) {
+            $errores['rol'] = 'Rol obligatorio';
+        }
+
+        # comprobamos la validación
+        if (!empty($errores)) {
+            // Errores de validación
+            $_SESSION['error'] = 'Formulario no validado';
+            $_SESSION['errores'] = $errores;
+
+            // Redireccionamos
+            header('location:' . URL . 'users/edit/' . $id);
+            exit();
+        }
+
+        // hashear la contraseña
+        if (!empty($password)) {
+            $passHash = password_hash($password, PASSWORD_BCRYPT);
+        } else {
+            $passHash = $userOG->password;
+        }
+
+        $user = new classUser(
+            null,
+            $nombre,
+            $email,
+            $passHash,
+            $rol
+        );
+
+        $this->model->update($id, $user, $rol);
+
+        $_SESSION['mensaje'] = 'Usuario actualizado correctamente';
+
+        header("Location:" . URL . "users");
+
+    }
+
+    function show($param = [])
+    {
+
+        session_start();
+
+        if (!isset($_SESSION['id'])) {
+            $_SESSION['mensaje'] = "Usuario no autentificado";
+
+            header("location:" . URL . "login");
+        } else if ((!in_array($_SESSION['id_rol'], $GLOBALS['user']['show']))) {
+            $_SESSION['mensaje'] = "Operación sin privilegios";
+            header('location:' . URL . 'users');
+        }
+
+        // Obtengo la id del user que quiero mostrar
+        $id = $param[0];
+
+        // Obtengo los datos del user mediante el modelo
+        $user = $this->model->read($id);
+
+        // Configuro las propiedades de la vista
+        $this->view->title = "Detalles del Usuario";
+        $this->view->user = $user;
+
+        // Cargo la vista de detalles del user
+        $this->view->render('users/show/index');
+
+    }
+
+
+    function order($param = [])
+    {
+
+        session_start();
+
+        if (!isset($_SESSION['id'])) {
+            $_SESSION['mensaje'] = "Usuario no autentificado";
+
+            header("location:" . URL . "login");
+        } else if ((!in_array($_SESSION['id_rol'], $GLOBALS['user']['order']))) {
+            $_SESSION['mensaje'] = "Operación sin privilegios";
+            header('location:' . URL . 'users');
+        }
+
+        # Obtengo el criterio de ordenación
+        $criterio = $param[0];
+
+        # Creo la propiedad "title" de la vista
+        $this->view->title = "Ordenar - Panel de Control users";
+
+        # Creo la propiedad users dentro de la vista
+        # Del modelo asignado al controlador ejecuto el método get()
+        $this->view->users = $this->model->order($criterio);
+
+        # Cargo la vista principal de users
+        $this->view->render('users/main/index');
+
+    }
+
+    function filter($param = [])
+    {
+
+        session_start();
+
+        if (!isset($_SESSION['id'])) {
+            $_SESSION['mensaje'] = "Usuario no autentificado";
+
+            header("location:" . URL . "login");
+        } else if ((!in_array($_SESSION['id_rol'], $GLOBALS['user']['filter']))) {
+            $_SESSION['mensaje'] = "Operación sin privilegios";
+            header('location:' . URL . 'users');
+        }
+
+        //Obtengo la expresión de filtrado
+        $expresion = $_GET['expresion'];
+
+        //Cambio la propiedad title de la vista
+        $this->view->title = "Buscar - Gestión users";
+
+        //Creamos la variable users dentro de la vista
+        //Esta variable ejecuta el método get() del modelo usersModel
+        $this->view->users = $this->model->filter($expresion);
+
+        //Cargo la vista index
+        $this->view->render('users/main/index');
+    }
+
+    function delete($param = [])
+    {
+        # Inicio o continúo la sesión
+        session_start();
+
+        //Comprobar si el usuario está identificado
+        if (!isset($_SESSION['id'])) {
+
+            $_SESSION['mensaje'] = "Usuario No Autentificado";
+            header("location:" . URL . "login");
+
+        } else if ((!in_array($_SESSION['id_rol'], $GLOBALS['user']['delete']))) {
+            $_SESSION['mensaje'] = "Operación sin privilegios";
+            header('location:' . URL . 'users');
+
+        } else {
+
+            //Obtenemos id del user
+            $iduser = $param[0];
+
+            // Eliminar el user
+            $this->model->delete($iduser);
+
+            //Generar mensaje
+            $_SESSION['notify'] = 'Usuario eliminado.';
+
+            header("Location:" . URL . "users");
+        }
+    }
+
+
+}
+
+?>
---
+++ tema-13/proyecto/proyectofinal/models/usersModel.php
@@ -0,0 +1,414 @@
+<?php
+
+/*
+    clientesModel.php
+
+    Modelo del controlador clientes
+
+    Definir los métodos de acceso a la base de datos
+    
+    - insert
+    - update
+    - select
+    - delete
+    - etc..
+*/
+
+class usersModel extends Model
+{
+
+    /*
+        Extrae los detalles  de los clientes
+    */
+    public function get()
+    {
+
+        try {
+
+            # comando sql
+            $sql = "SELECT 
+                        users.id,
+                        users.name,
+                        users.email,
+                        users.password
+                    FROM
+                        users
+                    ORDER BY 
+                        id";
+
+            # conectamos con la base de datos
+
+            // $this->db es un objeto de la clase database
+            // ejecuto el método connect de esa clase
+
+            $conexion = $this->db->connect();
+
+            # ejecutamos mediante prepare
+            $pdost = $conexion->prepare($sql);
+
+            # establecemos  tipo fetch
+            $pdost->setFetchMode(PDO::FETCH_OBJ);
+
+            #  ejecutamos 
+            $pdost->execute();
+
+            # devuelvo objeto pdostatement
+            return $pdost;
+
+        } catch (PDOException $e) {
+
+            include_once('template/partials/errorDB.php');
+            exit();
+
+        }
+    }
+
+    public function read($id)
+    {
+
+        try {
+            $sql = "SELECT 
+                        users.id,
+                        users.name,
+                        users.email,
+                        users.password                        
+                    FROM 
+                        users
+                    WHERE
+                        users.id = :id";
+
+            # Conectar con la base de datos
+            $conexion = $this->db->connect();
+
+
+            $pdoSt = $conexion->prepare($sql);
+
+            $pdoSt->bindParam(':id', $id, PDO::PARAM_INT);
+            $pdoSt->setFetchMode(PDO::FETCH_OBJ);
+            $pdoSt->execute();
+
+            return $pdoSt->fetch();
+
+        } catch (PDOException $e) {
+            include_once('template/partials/errorDB.php');
+            exit();
+        }
+
+    }
+
+    public function update(int $id, classUser $user, int $rol)
+    {
+        try {
+            $sql = "UPDATE users
+                    SET
+                        name = :name,
+                        email = :email,
+                        password = :password
+                    WHERE
+                        id = :id
+                    LIMIT 1";
+
+            $conexion = $this->db->connect();
+            $pdoSt = $conexion->prepare($sql);
+
+            $pdoSt->bindParam(':id', $id, PDO::PARAM_INT);
+            $pdoSt->bindParam(':name', $user->name, PDO::PARAM_STR, 50);
+            $pdoSt->bindParam(':email', $user->email, PDO::PARAM_STR, 50);
+            $pdoSt->bindParam(':password', $user->password, PDO::PARAM_STR, 60);
+
+            $pdoSt->execute();
+
+            // Actualizar el rol del usuario
+            $sqlRol = "UPDATE
+                            roles_users
+                        SET 
+                            role_id = :role_id
+                        WHERE 
+                            user_id = :user_id";
+
+            $stmtRol = $conexion->prepare($sqlRol);
+            $stmtRol->bindParam(':role_id', $rol);
+            $stmtRol->bindParam(':user_id', $id);
+            $stmtRol->execute();
+
+        } catch (PDOException $e) {
+            include_once('template/partials/errorDB.php');
+            exit();
+        }
+    }
+
+
+    public function order(int $criterio)
+    {
+        try {
+
+            # comando sql
+            $sql = "SELECT 
+                        users.id,
+                        users.name,
+                        users.email,
+                        users.password
+                    FROM
+                        users
+                    ORDER BY 
+                        :criterio";
+
+            $conexion = $this->db->connect();
+
+            # ejecutamos mediante prepare
+            $pdost = $conexion->prepare($sql);
+
+            $pdost->bindParam(':criterio', $criterio, PDO::PARAM_INT);
+
+            # establecemos  tipo fetch
+            $pdost->setFetchMode(PDO::FETCH_OBJ);
+
+            #  ejecutamos 
+            $pdost->execute();
+
+            # devuelvo objeto pdostatement
+            return $pdost;
+
+        } catch (PDOException $e) {
+
+            include_once('template/partials/errorDB.php');
+            exit();
+
+        }
+    }
+
+    public function filter($expresion)
+    {
+        try {
+            $sql = "SELECT 
+                        users.id,
+                        users.name,
+                        users.email,
+                        users.password
+                    FROM
+                        users
+                    WHERE
+                        CONCAT_WS(', ', 
+                                users.id,
+                                users.name,
+                                users.email,
+                                users.password) 
+                        LIKE :expresion
+                    ORDER BY 
+                        users.id
+                    ";
+
+            # Conectar con la base de datos
+            $conexion = $this->db->connect();
+
+            $pdost = $conexion->prepare($sql);
+
+            $pdost->bindValue(':expresion', '%' . $expresion . '%', PDO::PARAM_STR);
+            $pdost->setFetchMode(PDO::FETCH_OBJ);
+            $pdost->execute();
+
+            return $pdost;
+        } catch (PDOException $e) {
+            include_once('template/partials/errorDB.php');
+            exit();
+        }
+    }
+
+
+    public function delete($id)
+    {
+        try {
+            $sql = "DELETE
+                    FROM
+                        users
+                    WHERE 
+                        id = :id";
+
+            $conexion = $this->db->connect();
+
+            $pdoSt = $conexion->prepare($sql);
+            $pdoSt->bindParam(':id', $id, PDO::PARAM_INT);
+
+            $pdoSt->execute();
+
+        } catch (PDOException $e) {
+            include_once('template/partials/errorDB.php');
+            exit();
+        }
+    }
+
+
+    public function validateName($username)
+    {
+        if ((strlen($username) < 5) || (strlen($username) > 50)) {
+            return false;
+        }
+        return true;
+    }
+
+    #Validar password
+    public function validatePass($pass)
+    {
+        if ((strlen($pass) < 5) || (strlen($pass) > 50)) {
+            return false;
+        }
+        return true;
+    }
+
+    #Validar email unique
+    public function validateEmailUnique($email)
+    {
+
+        try {
+
+            $selectSQL = "SELECT * FROM users WHERE email = :email";
+            $pdo = $this->db->connect();
+            $pdost = $pdo->prepare($selectSQL);
+            $pdost->bindParam(':email', $email, PDO::PARAM_STR, 50);
+            $pdost->execute();
+            if ($pdost->rowCount() > 0)
+                return false;
+            else
+                return true;
+        } catch (PDOException $e) {
+
+            include_once('template/partials/errorDB.php');
+            exit();
+
+        }
+
+    }
+
+    # Creo nuevo usuario a partir de los datos de formulario de registro
+    public function crear($name, $email, $pass, $rol)
+    {
+        try {
+
+            $password_encriptado = password_hash($pass, CRYPT_BLOWFISH);
+
+            $sql = "INSERT INTO users VALUES (
+                 null,
+                :nombre,
+                :email,
+                :pass,
+                default,
+                default)";
+
+            $pdo = $this->db->connect();
+            $stmt = $pdo->prepare($sql);
+
+            $stmt->bindParam(':nombre', $name, PDO::PARAM_STR, 50);
+            $stmt->bindParam(':email', $email, PDO::PARAM_STR, 50);
+            $stmt->bindParam(':pass', $password_encriptado, PDO::PARAM_STR, 60);
+
+            $stmt->execute();
+
+            # Asignamos rol de registrado
+            $sql = "INSERT INTO roles_users VALUES (
+                null,
+                :user_id,
+                :role_id,
+                default,
+                default)";
+
+            # Obtener id del último usuario insertado
+            $ultimo_id = $pdo->lastInsertId();
+
+            $stmt = $pdo->prepare($sql);
+
+            $stmt->bindParam(':user_id', $ultimo_id);
+            $stmt->bindParam(':role_id', $rol);
+            $stmt->execute();
+
+        } catch (PDOException $e) {
+
+            include_once('template/partials/errorDB.php');
+            exit();
+
+        }
+    }
+
+    public function getUser($id)
+    {
+        try {
+            $sql = "SELECT
+                        *
+                    FROM
+                        users
+                    WHERE
+                        id = :id";
+
+            $pdo = $this->db->connect();
+            $pdoSt = $pdo->prepare($sql);
+            $pdoSt->bindParam(':id', $id, PDO::PARAM_INT);
+            $pdoSt->setFetchMode(PDO::FETCH_OBJ);
+
+            $pdoSt->execute();
+
+            return $pdoSt->fetch();
+
+        } catch (PDOException $e) {
+            require_once("template/partials/errorDB.php");
+            exit();
+        }
+    }
+
+    // Obtener los roles para crear un nuevo usuario
+    public function getRoles()
+    {
+        try {
+            $sql = "SELECT 
+                        roles.id, 
+                        roles.name 
+                    FROM 
+                        roles";
+
+            $conexion = $this->db->connect();
+            $pdost = $conexion->prepare($sql);
+            $pdost->execute();
+
+            return $pdost->fetchAll(PDO::FETCH_OBJ);
+        } catch (PDOException $e) {
+            include_once('template/partials/errorDB.php');
+            exit();
+        }
+    }
+
+
+    // Obtener el rol de un usuario para editarlo
+    public function userRol($id)
+    {
+        try {
+            $sql = "SELECT
+                        roles.id, roles.name
+                    FROM
+                        roles
+                    INNER JOIN
+                        roles_users
+                    ON
+                        roles.id = roles_users.role_id
+                    INNER JOIN
+                        users
+                    ON
+                        roles_users.user_id = users.id
+                    WHERE
+                        users.id = :id";
+
+            $pdo = $this->db->connect();
+            $pdoSt = $pdo->prepare($sql);
+            $pdoSt->bindParam(':id', $id, PDO::PARAM_INT);
+            $pdoSt->setFetchMode(PDO::FETCH_OBJ);
+
+            $pdoSt->execute();
+
+            return $pdoSt->fetch();
+
+        } catch (PDOException $e) {
+            require_once("template/partials/errorDB.php");
+            exit();
+        }
+    }
+
+}
+
+?>
