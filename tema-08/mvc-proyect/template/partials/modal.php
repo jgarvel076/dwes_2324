00e4c3968a8deb4cf04cd67c291f5d167eb1d995
Julian Garcia Velazquez <jgarvelju076@g.educaand.es>
2024-03-13 09:40:36 +0100

    actualizar modal

--- tema-08/mvc-proyect/template/partials/modal.php
+++ tema-08/mvc-proyect/template/partials/modal.php
@@ -4,7 +4,7 @@
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title">Subir Archivos</h5>
+                    <h5 class="modal-title">Subir Imagen</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
@@ -15,8 +15,8 @@
                     </div>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
                     <button type="submit" class="btn btn-primary">Subir</button>
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
                 </div>
             </div>
         </div>
